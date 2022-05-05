4eea2c7a5df398a8e832f713273a27d13a8ff445
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2022-05-05 13:18:53 +0300

    11

--- src/main/java/net/magforest/magforest/block/ModBlocks.java
+++ src/main/java/net/magforest/magforest/block/ModBlocks.java
@@ -242,7 +242,7 @@ public class ModBlocks extends Blocks
             () -> new Block(AbstractBlock.Properties.create(Material.CLAY)
                     .hardnessAndResistance(1).sound(SoundType.NETHER_BRICK).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
     public static final RegistryObject<Block> SLAB_CLAY_TILES = registerBlock("slab_clay_tiles",
-            () -> new Block(AbstractBlock.Properties.create(Material.CLAY)
+            () -> new SlabBlock(AbstractBlock.Properties.create(Material.CLAY)
                     .hardnessAndResistance(1).sound(SoundType.NETHER_BRICK).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
     public static final RegistryObject<Block> LAVA_ROCK_OBSIDIAN = registerBlock("lava_rock_obsidian",
             () -> new Block(AbstractBlock.Properties.create(Material.ROCK)
