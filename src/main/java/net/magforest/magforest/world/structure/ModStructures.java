f3a7fffe5c8491a205e8ab8a346c89f6d5a78d7e
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2022-05-19 13:43:00 +0300

    11

--- src/main/java/net/magforest/magforest/world/structure/ModStructures.java
+++ src/main/java/net/magforest/magforest/world/structure/ModStructures.java
@@ -32,7 +32,7 @@ public class ModStructures {
         setupMapSpacingAndLand(WIZARD_TOWER.get(),
                 new StructureSeparationSettings(30,15, 1234567890), true);
         setupMapSpacingAndLand(CRYSTAL_SAVE.get(),
-                new StructureSeparationSettings(25,15, 1234567890), true);
+                new StructureSeparationSettings(15,7, 1234567890), true);
     }
 
     public static <F extends Structure<?>> void setupMapSpacingAndLand(F structure, StructureSeparationSettings structureSeparationSettings,
