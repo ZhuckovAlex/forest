977706c4aff1bcf6e74f217565d6a1dd759256e2
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2022-05-25 09:21:46 +0300

    11

--- src/main/java/net/magforest/magforest/item/custom/ModThrowablePotionItem.java
+++ src/main/java/net/magforest/magforest/item/custom/ModThrowablePotionItem.java
@@ -1,5 +1,7 @@
 package net.magforest.magforest.item.custom;
 
+import net.magforest.magforest.magforest;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PotionEntity;
 import net.minecraft.item.Item;
@@ -8,7 +10,13 @@ import net.minecraft.item.PotionItem;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ModThrowablePotionItem extends Item {
     public ModThrowablePotionItem(Item.Properties properties) {
@@ -34,5 +42,36 @@ public class ModThrowablePotionItem extends Item {
         }
 
         return ActionResult.func_233538_a_(itemstack, worldIn.isRemote());
+    }
+    @SubscribeEvent
+    public static void onRightClickItem(PlayerInteractEvent.RightClickItem event) {
+        PlayerEntity entity = event.getPlayer();
+        if (event.getHand() != entity.getActiveHand()) {
+            return;
+        }
+        double i = event.getPos().getX();
+        double j = event.getPos().getY();
+        double k = event.getPos().getZ();
+        IWorld world = event.getWorld();
+        Map<String, Object> dependencies = new HashMap<>();
+        dependencies.put("x", i);
+        dependencies.put("y", j);
+        dependencies.put("z", k);
+        dependencies.put("world", world);
+        dependencies.put("entity", entity);
+        dependencies.put("event", event);
+        executeProcedure(dependencies);
+    }
+
+
+    public static void executeProcedure(Map<String, Object> dependencies) {
+        if (dependencies.get("entity") == null) {
+            if (!dependencies.containsKey("entity"))
+                magforest.LOGGER.warn("Failed to load dependency entity for procedure PotionGlotok!");
+            return;
+        }
+        Entity entity = (Entity) dependencies.get("entity");
+        if (!entity.world.isRemote())
+            entity.remove();
     }
 }
--- src/main/java/net/magforest/magforest/item/custom/PotionLove.java
+++ src/main/java/net/magforest/magforest/item/custom/PotionLove.java
@@ -1,5 +1,8 @@
 package net.magforest.magforest.item.custom;
 
+import net.magforest.magforest.magforest;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -7,7 +10,13 @@ import net.minecraft.util.Hand;
 
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PotionLove extends ModThrowablePotionItem{
 
@@ -19,4 +28,6 @@ public class PotionLove extends ModThrowablePotionItem{
         worldIn.playSound((PlayerEntity)null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), SoundEvents.ENTITY_SPLASH_POTION_THROW, SoundCategory.PLAYERS, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
+
+
 }
