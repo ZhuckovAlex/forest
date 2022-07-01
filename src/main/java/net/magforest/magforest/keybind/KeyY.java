2392fada1b2fe65986852ae58fdb8621694ac589
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2022-07-01 09:25:18 +0300

    11

---
+++ src/main/java/net/magforest/magforest/keybind/KeyY.java
@@ -0,0 +1,68 @@
+package net.magforest.magforest.keybind;
+import java.util.function.Supplier;
+
+import net.magforest.magforest.MagforestModElements;
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.client.settings.KeyBinding;
+
+@MagforestModElements.ModElement.Tag
+public class KeyY extends MagforestModElements.ModElement{
+    @OnlyIn(Dist.CLIENT)
+    private KeyBinding keys;
+
+    public KeyY(MagforestModElements instance) {
+        super(instance, 24);
+        elements.addNetworkMessage(KeyBindingPressedMessage.class, KeyBindingPressedMessage::buffer, KeyBindingPressedMessage::new,
+                KeyBindingPressedMessage::handler);
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void initElements() {
+        keys = new KeyBinding("key.magforest.key_y", GLFW.GLFW_KEY_Y, "key.categories.misc");
+        ClientRegistry.registerKeyBinding(keys);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public void onKeyInput(InputEvent.KeyInputEvent event) {
+    }
+
+    public static class KeyBindingPressedMessage {
+        int type, pressedms;
+
+        public KeyBindingPressedMessage(int type, int pressedms) {
+            this.type = type;
+            this.pressedms = pressedms;
+        }
+
+        public KeyBindingPressedMessage(PacketBuffer buffer) {
+            this.type = buffer.readInt();
+            this.pressedms = buffer.readInt();
+        }
+
+        public static void buffer(KeyBindingPressedMessage message, PacketBuffer buffer) {
+            buffer.writeInt(message.type);
+            buffer.writeInt(message.pressedms);
+        }
+
+        public static void handler(KeyBindingPressedMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+            NetworkEvent.Context context = contextSupplier.get();
+            context.enqueueWork(() -> {
+            });
+            context.setPacketHandled(true);
+        }
+    }
+
+}
